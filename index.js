5811a76e5ae7628bce87a303f09346b9b79c2b12
emmett <emmett@emmett.attlocal.net>
2021-01-03 20:26:07 -0600

    remove service worker

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 
 import './scss/application.scss';
 import DemoBar from './demobar';
-import * as serviceWorker from './serviceWorker';
 import * as variables from './variables';
 
 import Loadable from 'react-loadable';
